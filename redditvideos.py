a19914a50e4666c0d0be239700f00f5652838071
namrak <namrak@gmail.com>
2020-02-07 17:57:34 -0700

    Automatic resolution for v.redd.it urls

--- redditvideos.py
+++ redditvideos.py
@@ -9,6 +9,7 @@ import sys
 import clipboard
 from urllib.request import urlopen, HTTPError
 import platform
+from moviepy.editor import *
 
 osys = platform.system()
 real_path = os.path.dirname(os.path.realpath(__file__))
@@ -16,10 +17,14 @@ real_path = os.path.dirname(os.path.realpath(__file__))
 class RedditDownloader:
     def __init__(self):
         self.url = clipboard.paste()
-        if isinstance(self.url, str) and (str(self.url).startswith("https://www.reddit.com") or str(self.url).startswith("https://old.reddit.com")):
-            #download video directly, no ui
-            self.reddit_downloader()
-        else:
+        self.download_completed = False
+        # use UA headers to prevent 429 error
+        self.headers = {
+                "User-Agent": "My User Agent 1.0",
+                "From": "testyouremail@domain.com"
+        }
+        self.determine_url()
+        if not self.download_completed:
             #setup UI
             self.win = Tk()
             self.win.attributes("-fullscreen", False)
@@ -40,29 +45,37 @@ class RedditDownloader:
 
     def get_url_and_download(self):
         self.url = self.urlInput.get()
+        self.determine_url()
+
+    def determine_url(self):
+        if isinstance(self.url, str) and (str(self.url).startswith("https://www.reddit.com") or str(self.url).startswith("https://old.reddit.com")):
+            self.reddit_downloader()
+        elif isinstance(self.url, str) and str(self.url).startswith("https://v.redd.it"):
+            # resolve url and request with correct url
+            self.resolve_vreddit_url()
+
+    def resolve_vreddit_url(self):
+        
+        self.url = requests.get(self.url, headers=self.headers).url
         self.reddit_downloader()
 
     def reddit_downloader(self):
         try:
-            # use UA headers to prevent 429 error
-            headers = {
-                "User-Agent": "My User Agent 1.0",
-                "From": "testyouremail@domain.com"
-            }
             json_url = self.url + ".json"
-            data = requests.get(json_url, headers=headers).json()
+            data = requests.get(json_url, headers=self.headers).json()
             media_data = data[0]["data"]["children"][0]["data"]["media"]
             title = data[0]["data"]["children"][0]["data"]["title"]
 
             # sanitize title
-            invalid = """<>:"/\|?*. """
+            invalid = """<>:"/\|?*.'"""
             for char in invalid:
-                title = title.replace(char, "")
+                self.title = title.replace(char, "")
 
-            video_path  = os.path.join(real_path,"Output",title)+".mp4"
-            folder_path = os.path.join(real_path,"Output")
-            if not os.path.exists(folder_path):
-                os.makedirs(folder_path)
+            self.video_path  = os.path.join(real_path,"Output",self.title)+".mp4"
+            self.test_video_path  = os.path.join(real_path,"Output","2"+self.title)+".mp4"
+            self.folder_path = os.path.join(real_path,"Output")
+            if not os.path.exists(self.folder_path):
+                os.makedirs(self.folder_path)
 
             video_url = media_data["reddit_video"]["fallback_url"]
             audio_url = video_url.split("DASH_")[0] + "audio"
@@ -73,7 +86,7 @@ class RedditDownloader:
             except HTTPError as err:
                 if err.code == 403:
                     # no audio, download video to Output directly
-                    os.system("curl -o {} {}".format(video_path, video_url))
+                    os.system("curl -o {} {}".format(self.video_path, video_url))
                     messagebox.showinfo("Success", "{} was downloaded.".format(self.url))
                     return
 
@@ -82,11 +95,11 @@ class RedditDownloader:
 
             if osys == "Windows":
                 # add ffmpeg bin directory to PATH environment variable or full path to binary in the next line
-                os.system("ffmpeg.exe -y -i video.mp4 -i audio.wav -c:v copy -c:a aac -strict experimental \""+video_path+"\"")
-                os.system("start " + folder_path)
+                os.system("ffmpeg.exe -y -i video.mp4 -i audio.wav -c:v copy -c:a aac -strict experimental \""+self.video_path+"\"")
+                os.system("start " + self.folder_path)
             elif osys == "Linux":
-                os.system("ffmpeg -y -i video.mp4 -i audio.wav -c:v copy -c:a aac -strict experimental '"+video_path+"'")
-                os.system("xdg-open " + folder_path)
+                os.system("ffmpeg -y -i video.mp4 -i audio.wav -c:v copy -c:a aac -strict experimental '"+self.video_path+"'")
+                os.system("xdg-open " + self.folder_path)
             else:
                 messagebox.showerror("Error", "{} is not supported".format(osys))
                 return
